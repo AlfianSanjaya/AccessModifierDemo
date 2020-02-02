52d7b88bb94ef6216336d5966b58c3ea8710c7b4
Alfian Sanjaya <s113573@ap.be>
2020-02-02 19:37:04 +0100

    titel toegevoegd

---
+++ AccessModifierDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AccessModifierDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Access Modifier Demo");
+
+            Console.WriteLine("**********************");
+        }
+    }
+}
